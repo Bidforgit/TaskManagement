27c1041b645f5bbc6b5b34649500850acbcadc12
Bidforgit <anuar.karimow@gmail.com>
2023-11-03 19:30:40 +0600

    jbdcapi + postgre + of tasks and users

--- src/jbdcapiPostgres/Main.java
+++ src/jbdcapiPostgres/Main.java
@@ -3,40 +3,32 @@ package jbdcapiPostgres;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.Timestamp;
-import java.util.ArrayList;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.sql.SQLException;
-import java.security.SecureRandom;
-import java.security.spec.KeySpec;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import java.security.*;
+import java.text.ParseException;
+
 import java.util.Scanner;
-
 import org.mindrot.jbcrypt.BCrypt;
-
-class UserService {
-    private DatabaseMetaData DatabaseConnection;
-
-
-}
-
 
 public class Main {
 
     private static Connection connection;
-    private static String url = "jdbc:postgresql://localhost:5432/northwind";
-    private static String user = "postgres";
-    private static String password = "123";
+    private static final String user = "postgres";
+    private static final String password = "123";
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ParseException {
         Scanner sc= new Scanner(System.in);
 
         // login    password:
         //1 loginim  parol
         //2 test1    passworr
+        //3 user1    user1
 
         try{
-        connection = DriverManager.getConnection(url,user,password);
+            String url = "jdbc:postgresql://localhost:5432/northwind";
+            connection = DriverManager.getConnection(url,user,password);
             System.out.println("Connected");
         }catch (Exception e){
             e.printStackTrace();
@@ -48,7 +40,7 @@ public class Main {
         sc.nextLine();
 
         String login = null;
-        String pass = null;
+        String pass;
         switch (val) {
             case 1:
                 System.out.print("Enter your username: ");
@@ -60,11 +52,8 @@ public class Main {
                 System.out.println(authenticated);
 
                 while (!authenticated) {
-                    if (authenticated) {
-                        System.out.println("You are logged in.");
-                    } else {
-                        System.out.println("Authentication failed. PLS try gain!");
-                    }
+                    System.out.println("Authentication failed. PLS try gain!");
+
                     System.out.print("Enter your username: ");
                     login = sc.nextLine();
                     System.out.print("Enter your password: ");
@@ -74,9 +63,11 @@ public class Main {
                 }
                 break;
             case 2:
+                System.out.print("Enter your username: ");
                 login = sc.nextLine();
+                System.out.print("Enter your password: ");
                 pass = sc.nextLine();
-                registerUser(login,pass);
+                System.out.println(registerUser(login,pass));
                 System.out.println("You re registered");
                 break;
         }
@@ -94,23 +85,24 @@ public class Main {
             System.out.println("to EXIT -> 6:");
 
             val = sc.nextInt();
-            sc.nextLine();
             switch (val) {
                 case 1:
-                   title= sc.nextLine();
+                    sc.nextLine();
+                    title= sc.nextLine();
                     desc = sc.nextLine();
                     creation = sc.nextLine();
                     status = sc.nextLine();
                     addTask(title,desc,creation,status,login);
                     break;
                 case 2:
-
+                    int task_id = sc.nextInt();
+                    sc.nextLine();
                     title= sc.nextLine();
                     desc = sc.nextLine();
                     creation = sc.nextLine();
                     status = sc.nextLine();
-                    updateTask(1,title,desc,creation,status,login);
-
+                    updateTask(task_id,title,desc,creation,status,login);
+                    //2023-12-12 12:21:23.000000
                     break;
                 case 3:
                     getTasksFromUser(login);
@@ -122,6 +114,7 @@ public class Main {
                     getTasksFromUser(login);
                     break;
                 case 5:
+                    sc.nextLine();
                     status = sc.nextLine();
                     filterTasksByStatus(login,status);
                     break;
@@ -136,7 +129,7 @@ public class Main {
     public static boolean registerUser(String username, String password) throws SQLException {
         String salt = BCrypt.gensalt(); // Генерируем соль
         String hashedPassword = BCrypt.hashpw(password, salt); // Хэшируем пароль
-        DatabaseMetaData DatabaseConnection;
+
         String query = "INSERT INTO users (username, password_hash, salt) VALUES (?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, username);
@@ -147,35 +140,57 @@ public class Main {
 
     }
 
-    public static boolean addTask(String title, String description,String creation_date,String status,String username) throws SQLException {
+    public static void addTask(String title, String description, String creation_date, String status, String username) throws SQLException {
         String query = "INSERT INTO tasks (title, description, creation_date,status,user_id) VALUES (?, ?, ?,?, " +
                 "(SELECT user_id FROM users WHERE username = ?)) ";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, title);
             preparedStatement.setString(2, description);
-            preparedStatement.setTimestamp(3, Timestamp.valueOf(creation_date));
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date parsedDate = dateFormat.parse(creation_date);
+                Timestamp timestamp = new Timestamp(parsedDate.getTime());
+                preparedStatement.setTimestamp(3, timestamp);
+            } catch (java.text.ParseException e) {
+                // Handle the case where the date is not in the correct format
+                System.err.println("Invalid date format: " + creation_date);
+                // You can choose to handle or log the error appropriately
+            }
             preparedStatement.setString(4, status);
             preparedStatement.setString(5, username);
-            return preparedStatement.executeUpdate() == 1;
+            preparedStatement.executeUpdate();
         }
     }
 
-    public static boolean updateTask(int task_id,String title, String description,String creation_date,String status,String username) throws SQLException {
+    public static boolean updateTask(int task_id, String title, String description, String creation_date, String status, String username) throws SQLException, ParseException {
         String query = "UPDATE tasks\n" +
                 "SET title = ?, description = ?, creation_date = ?, status = ?\n" +
-                "WHERE task_id = ? and user_id = (SELECT user_id FROM users WHERE username = ?);\n";
+                "WHERE task_id = ? and user_id = (SELECT user_id FROM users WHERE username = ?);";
+
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, title);
             preparedStatement.setString(2, description);
-            preparedStatement.setTimestamp(3, Timestamp.valueOf(creation_date));
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date parsedDate = dateFormat.parse(creation_date);
+                Timestamp timestamp = new Timestamp(parsedDate.getTime());
+                preparedStatement.setTimestamp(3, timestamp);
+            } catch (java.text.ParseException e) {
+                // Handle the case where the date is not in the correct format
+                System.err.println("Invalid date format: " + creation_date);
+                // You can choose to handle or log the error appropriately
+            }
+
             preparedStatement.setString(4, status);
-            preparedStatement.setInt(5, task_id);  // Set task_id
+            preparedStatement.setInt(5, task_id);
             preparedStatement.setString(6, username);
 
             return preparedStatement.executeUpdate() == 1;
         }
     }
-    public static void dropTask(int task_id) throws SQLException {
+
+    public static void dropTask(int task_id) {
         try{
             Statement statement = connection.createStatement();
             String query = "DELETE FROM tasks \n" +
@@ -186,7 +201,7 @@ public class Main {
             e.printStackTrace();
         }
     }
-    public static void getTasksFromUser(String username) throws SQLException {
+    public static void getTasksFromUser(String username){
         try {
 
             Statement statement = connection.createStatement();
@@ -209,7 +224,7 @@ public class Main {
             e.printStackTrace();
         }
     }
-    public static void filterTasksByStatus(String username, String status) throws SQLException {
+    public static void filterTasksByStatus(String username, String status) {
         try {
             String query = "SELECT * FROM tasks WHERE status = ? " +
                     "and user_id = (SELECT user_id FROM users WHERE username = ?) GROUP BY task_id";
@@ -234,9 +249,6 @@ public class Main {
         }
     }
 
-
-
-
     public static boolean authenticateUser(String username, String password) throws SQLException {
         String query = "SELECT password_hash, salt FROM users WHERE username = ?";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
@@ -244,20 +256,12 @@ public class Main {
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
                 String hashedPassword = resultSet.getString("password_hash");
-                String salt = resultSet.getString("salt");
-
-
-                // Use BCrypt to check if the provided password matches the stored hash and salt.
-                String hashed = BCrypt.hashpw(password, BCrypt.gensalt(10));
 
                 return BCrypt.checkpw(password, hashedPassword);
             }
-
             return false;
         }
     }
-
-
 
 }
 
